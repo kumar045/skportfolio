4a9cedc47a7e22fdfd6b285e991f53fd73956df0
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2020-06-01 19:39:08 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -132,8 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
