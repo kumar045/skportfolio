63289e642c063facdf9663c843f074e7c25bc94d
shivam kumar <kumarshivam066@gmail.com>
2020-06-01 23:09:41 +0530

    running

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1cq*4)t(i5fjjzm73dp0i64v&3g8kw@&79k883x4kl@l5^1rs&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shivamkportfolio.herokuapp.com']
 
@@ -127,11 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-'''
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-'''
+
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
 
