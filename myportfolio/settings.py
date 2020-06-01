86fdbe4755e923a3f243f47aff891f77a3abc369
shivam kumar <kumarshivam066@gmail.com>
2020-06-01 23:05:45 +0530

    running

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+'''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+'''
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
 
