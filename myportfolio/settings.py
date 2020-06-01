797d70557c03440691bedbac67ee4bc22c1efdf1
shivam kumar <kumarshivam066@gmail.com>
2020-06-02 00:17:53 +0530

    running

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
-import dj_database_url
-import whitenoise
-
+import heroku as heroku
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,10 +135,9 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = whitenoise.storage.CompressedManifestStaticFilesStorage
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 
 
