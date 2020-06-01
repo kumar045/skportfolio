6f9bd5d0d08e39cc8ae58c741f0e770deb45eb02
shivam kumar <kumarshivam066@gmail.com>
2020-06-01 23:40:34 +0530

    commit changes

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -7,15 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+
 import os
-from django.core.wsgi import get_wsgi_application
-import os
-#from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
 
 
 
